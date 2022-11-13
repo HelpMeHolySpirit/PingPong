a1407642b244f7b9b41f783b45dbbb8630e48bce
HelpMeHolySpirit <mz.piatkowski@gmail.com>
2022-11-13 10:29:37 +0100

    initial commit

---
+++ src/GamePanel.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class GamePanel extends JPanel implements Runnable{
+    static final int GAME_WIDTH = 1000;
+
+    public GamePanel(){
+
+    }
+    public void newBall(){
+
+    }
+    public void newPaddles(){
+
+    }
+    public void paint(Graphics g){
+
+    }
+    public void draw(Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkCollision(){
+
+    }
+    public void run(){
+
+    }
+
+    public class AL extends KeyAdapter {
+        public void keyPressed(KeyEvent e){
+
+        }
+        public void keyReleased(KeyEvent e){
+
+        }
+    }
+}
